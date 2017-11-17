7d929fb63de7b492d8b73ef2707244277b5cabda
Omar Yacoub <omar_yacoub@outlook.com>
2017-11-17 00:56:51 -0500

    simple program in JS

---
+++ JsStalker/js/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your name?");
+var lastName = prompt("What is your Last Name?");
+var age = prompt("How old are you?");
+
+var fullName = firstName + " " + lastName;
+
+console.log("Your full name is " + fullName);
+console.log("Your age is " + age + " years old.");
