5b0eb9f86722c94151adb8243acaedaabe18fb29
Omar Yacoub <omar_yacoub@outlook.com>
2017-11-26 23:35:35 -0500

    write code for add list and quit

--- toDoList/js/script.js
+++ toDoList/js/script.js
@@ -1,2 +1,19 @@
-var input = prompt("What do you want to do?"); 
+var input = prompt("What do you want to select?");
+var toDoList = ["eat"];
 
+
+while (input !== "quit") {
+    if (input === "list") {
+        toDoList.forEach(function(list,index){  
+            console.log(index + ": " + list);
+        })
+    } else if (input === "add"){
+        input = prompt("What to do you want add?");
+        toDoList.push(input);
+        console.log(input + " added to the to do list");
+    }
+
+    input = prompt("What do you want to select?");
+}
+
+console.log("you exit the app!");
