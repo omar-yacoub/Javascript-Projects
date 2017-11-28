9ab85674d33fb60581e907145efb6c139a8915b5
Omar Yacoub <omar_yacoub@outlook.com>
2017-11-27 22:42:07 -0500

    refactor the code and add delete option

--- toDoList/js/script.js
+++ toDoList/js/script.js
@@ -4,16 +4,35 @@ var toDoList = ["eat"];
 
 while (input !== "quit") {
     if (input === "list") {
-        toDoList.forEach(function(list,index){  
-            console.log(index + ": " + list);
-        })
-    } else if (input === "add"){
-        input = prompt("What to do you want add?");
-        toDoList.push(input);
-        console.log(input + " added to the to do list");
+        listTodo();
+    } else if (input === "new"){
+        newTodo();
+    }  else if (input === "delete"){
+        deteleTodo();
     }
-
+    // ask again
     input = prompt("What do you want to select?");
 }
-
 console.log("you exit the app!");
+
+function listTodo(){
+    console.log("**********");
+    toDoList.forEach(function(list,index){  
+        console.log(index + ": " + list);
+    })
+    console.log("**********");
+}
+
+function newTodo(){
+    var newTodo = prompt("What to do you want add?");
+    toDoList.push(newTodo);
+    console.log(newTodo + " added to the to do list");
+}
+
+function deteleTodo(){
+    // ask for index of todo to be deleted
+    var index = prompt("What is index do you want to Delete? ")
+    // delete that to do
+    toDoList.splice(index,1);
+    console.log("index (" + index +") is deleted.");
+}
